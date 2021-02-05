3f79bb29b732aba9e2c79feb70f24182af32d69a
Harkline <35963363+Harkline@users.noreply.github.com>
2021-02-05 16:48:07 +0100

    Modification - agent.py
    -récupère les types de processeurs
    -récupère la fréquence max, min, actuelle du premier processeur uniquement
    -Traduit anglais -> français (code et commentaire)
    -suppression de print de debug

--- agent.py
+++ agent.py
@@ -2,69 +2,49 @@
 
 import platform,socket,psutil,requests,json,csv,os
 
-# writing a function to convert bytes to GigaByte
-def bytes_to_GB(bytes):
-    gb = bytes/(1024*1024*1024)
-    gb = round(gb, 2)
-    return gb
+# Converti octets en Gigaoctets
+def octetsAGigaoctets(bytes):
+    go = bytes/(1024*1024*1024)
+    go = round(go, 2)
+    return go
     
-#On récupère les informations demandés
+#Structure principal ou toutes les informations sont stockés
 info={}
 
 
 ################################################Caractéristiques########################################################################
 info['nomHote']=socket.gethostname() #Nom de l'hôte 
 info['platforme']=platform.system() #Système d'exploitation
-# getting the system up time from the uptime file at proc directory
+# Obtient le temps d'activité en regardant dans le fichier /proc/uptime
 with open("/proc/uptime", "r") as f:
     tempsActif = f.read().split(" ")[0].strip()
 tempsActif = int(float(tempsActif))
-uptime_heures = tempsActif // 3600
-uptime_minutes = (tempsActif % 3600) // 60
-info['tempsActif'] = str(uptime_heures) + ":" + str(uptime_minutes) + " heures" #temps d'activité (uptime)
+tempsActifHeures = tempsActif // 3600
+tempsActifMinutes = (tempsActif % 3600) // 60
+info['tempsActif'] = str(tempsActifHeures) + ":" + str(tempsActifMinutes) + " heures" #temps d'activité (uptime)
 info['noyau']=platform.release() #Noyau
 
-#info['CPU']=(str(psutil.cpu_percent()) + ", " + str(psutil.virtual_memory()), psutil.cpu_freq())
-
-
-
-"""
-# Displaying The CPU information
-
-print("\n\t\t\t CPU Information\n")
-
-# This code will print the number of CPU cores present
-info['CpuNbrCoeursPhysiques'] = psutil.cpu_count(logical=False)
-info['CpuNbrCoeursTotal'] = psutil.cpu_count(logical=True)
-print ("FREQUENCE : " + str(psutil.cpu_freq()))
-
-
-# This will print the maximum, minimum and current CPU frequency
-cpu_frequency = psutil.cpu_freq()
-print(f"[+] Max Frequency : {cpu_frequency.max:.2f}Mhz")
-print(f"[+] Min Frequency : {cpu_frequency.min:.2f}Mhz")
-print(f"[+] Current Frequency : {cpu_frequency.current:.2f}Mhz")
-print("\n")
-
-# This will print the usage of CPU per core
-for i, percentage in enumerate(psutil.cpu_percent(percpu=True, interval=1)):
-    print(f"[+] CPU Usage of Core {i} : {percentage}%")
-print(f"[+] Total CPU Usage : {psutil.cpu_percent()}%")
-
-
-# reading the cpuinfo file to print the name of
-# the CPU present
+# Lit les information du fichier cpuinfo pour afficher le nom du processeur
 with open("/proc/cpuinfo", "r") as f:
-    file_info = f.readlines()
+    infoFichier = f.readlines()
 
-cpuinfo = [x.strip().split(":")[1] for x in file_info if "model name"  in x]
+nbreProc=0 #Permet de différencier les processeurs s'il y en a plusieurs
+itemTampon="" #Permet de vérifier pendant le traitement si le thread vient du même processeur
+cpuinfo = [x.strip().split(":")[1] for x in infoFichier if "model name"  in x]
 for index, item in enumerate(cpuinfo):
-    print("[+] Processor " + str(index) + " : " + item)
+    #Alternative vu que la fréquence récupéré n'est celle que du premier processeur
+    if nbreProc == 0:
+        frequenceProcesseur = psutil.cpu_freq()
+        #Nom du processeur, fréquence max, fréquence min, fréquence actuelle
+        info["Processeur" + str(nbreProc)] = item, frequenceProcesseur.max, frequenceProcesseur.min, frequenceProcesseur.current
+        itemTampon = item
+        nbreProc+=1
+    #Si un autre processeur est détecté
+    elif item != itemTampon:
+        info["Processeur" + str(nbreProc)] = item
+        itemTampon = item
+        nbreProc+=1
 
-    #Processeurs
-#info['platformeVersion']=platform.version()
-#info['architecture']=platform.machine()
-"""
 
 
 ################################################Métriques########################################################################
@@ -72,51 +52,47 @@ for index, item in enumerate(cpuinfo):
 informationDisque = []
 partitionsDisque = psutil.disk_partitions()
 compteur = 0 #Compteur pour différencier les partitions
-#print(partitionsDisque)
 
 #Permet d'afficher pour chaque partition les infos qu'on veut
-#TO DO, créer une liste avec toute les infos pour une partition, une liste par partition !
 for partition in partitionsDisque:
-    #if not bufferInfoPartition or partition.device != bufferInfoPartition.device :
-    bufferInfoPartition = {}
-    #bufferInfoPartition['cheminDevice']=partition.device
-    #bufferInfoPartition['pointDeMontage']=partition.mountpoint
-    #bufferInfoPartition['systemeFichier']=partition.fstype
+    tamponInfoPartition = {}
     compteur += 1
     usagePartitionStr = "usagePartition" + str(compteur) #Permet de différencier les partitions à l'aide d'un simple compteur
-    bufferInfoPartition[usagePartitionStr]=psutil.disk_usage(partition.mountpoint)
-    informationDisque.append(bufferInfoPartition)
+    tamponInfoPartition[usagePartitionStr] = psutil.disk_usage(partition.mountpoint)
+    informationDisque.append(tamponInfoPartition)
 
-info['informationsPartitions'] = informationDisque #Les informations des partitions (ATTENTION : PB potentiel pour OLIMALT pour gérer mes informations
+info['informationsPartitions'] = informationDisque #Les informations des partitions (ATTENTION : PB potentiel pour OLIMALT pour gérer mes informations)
 info['chargeCPU'] = psutil.cpu_percent() #En pourcentage
 
 
-####Mémoire
+######Mémoire
 
-virtual_memory = psutil.virtual_memory()
-virtual_memory.buffers #bytes_to_GB(virtual_memory.buffers) Fonction utile, mais valeur trop faible pour être utile
+memoireVirtuel = psutil.virtual_memory()
+memoireVirtuel.buffers #octetsAGigaoctets(virtual_memory.buffers) Fonction utile, mais valeur trop faible pour être utile
 
-info['memoireTotal'] =virtual_memory.total
-info['memoireFree'] =virtual_memory.free
-info['memoireOccupée']=virtual_memory.used
-info['memoireBuffer'] =virtual_memory.buffers #7 buffers, 8 cache (mémoire) EN BYTES PENSER A CONVERTIR
-info['memoireCache'] =virtual_memory.cached
+info['memoireTotal'] = memoireVirtuel.total
+info['memoireFree'] = memoireVirtuel.free
+info['memoireOccupée']= memoireVirtuel.used
+info['memoireBuffer'] = memoireVirtuel.buffers #7 buffers, 8 cache (mémoire) EN BYTES PENSER A CONVERTIR
+info['memoireCache'] = memoireVirtuel.cached
 
 ######CSV reader
-#https://www.commentcamarche.net/faq/2382-python-lire-et-ecrire-des-fichiers-csv
-cr = csv.reader(open("config.csv","r"))
-for row in cr:#Pour chaque ligne du fichier CSV
-    for service in row: #On récupère les services à chaque ligne du fichier CSV
-        isRunning = os.system('service ' +service+ ' status'); #On vérifie son statut
-        if isRunning = 0:
-            info[service]= 'Actif'
+fichierCSV = csv.reader(open("config.csv","r"))
+for ligne in fichierCSV:#Pour chaque ligne du fichier CSV
+    for service in ligne: #On récupère les services à chaque ligne du fichier CSV
+        estEnMarche = os.system('service ' +service+ ' status'); #On vérifie son statut
+        if estEnMarche == 0:
+            info[service] = 'Actif'
         else: #Code 768 signifie qu'il est arrêté, mais si le code est différent cela veut dire que le service n'existe pas, donc il est inactif
-            info[service]= 'Inactif'
+            info[service] = 'Inactif'
         
     
-
+#Debug
 print (info)#affichage des informations listés
 
+
+################################################Envoie de données (POST)########################################################
 url = "http://192.168.3.25:8097"
 info = json.dumps(info)
 #r = requests.post(url, data = info) #192.168.3.25  8011  https://serveur.requestcatcher.com
+
